3e847f06dfe1e169abbcce1c48348737ad8c28eb
Motta <agoncale@emeal.nttdata.com>
2025-04-07 18:31:28 -0300

    fix: usando action

--- pages/api/ready.ts
+++ pages/api/ready.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest,
+    res: NextApiResponse) {
     res.status(200).json({ status: 'ok' });
 }
